90ef69c72bef7c67ec24c7cc74d983cf6a3cd5b6
steve nguyen <vnsgbt@me.com>
2016-05-18 22:08:27 -0500

    add serve static file

--- hello/hello.go
+++ hello/hello.go
@@ -1,14 +1,15 @@
 package hello
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func init() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler)
+	http.Handle("/hello/dist/", http.FileServer(http.Dir(".")))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+	fmt.Fprint(w, "Hello, world!")
 }
